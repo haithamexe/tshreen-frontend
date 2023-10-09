f7219f6d0fb6189acc32c38ad7e03a22eddec24f
Haitham <haitham9559@gmail.com>
2023-10-09 14:53:47 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import FolllowCard from "./components/FolllowCard";
+import FollowTriangles from "./components/FollowTriangles";
 
 function App() {
+  const ary = ["ajshd", "asjhd", "asjhd", "asdasd", "asdasdasdas"];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* <FolllowCard /> */}
+      <FollowTriangles />
+      <div className="block-section">
+        <div className="block">
+          {ary.map((name, index) => (
+            <h1 className="name, engrave" key={index}>
+              {name}
+            </h1>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
